a1cf828c0c5cf0b0e5d0e7bb9367bd4334a4b272
Heather Guarnera <hguarnera@wooster.edu>
2025-07-02 11:16:49 -0400

    Overhaul: fix basepath and asset loading

--- site/next.config.ts
+++ site/next.config.ts
@@ -1,10 +1,10 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'export', 
+const nextConfig = {
+  output: 'export',
+  basePath: isProd ? '/hguarnera.com' : '',
+  assetPrefix: isProd ? '/hguarnera.com/' : '',
 };
-
-module.exports = nextConfig;
 
 export default nextConfig;

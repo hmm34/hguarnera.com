c6e5d4bd9e93061e2112a638c67e8ddc2e29813b
Heather Guarnera <hguarnera@wooster.edu>
2025-07-02 11:24:39 -0400

    Overhaul: remove basepath to fix double asset path issue

--- site/next.config.ts
+++ site/next.config.ts
@@ -1,10 +1,5 @@
-/** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/hguarnera.com' : '',
-  assetPrefix: isProd ? '/hguarnera.com/' : '',
 };
 
 export default nextConfig;
